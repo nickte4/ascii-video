59ce46e82b60c1c48de43f7c97f70a5153d189aa
nickte4 <ntopacio25@gmail.com>
2022-07-19 22:55:23 -0700

    Rearrange p5 library dir

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 
+let picture;
+
+function preload() {
+  picture = loadImage("./images/small-dog.jpg");
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {}
+function draw() {
+  background(220);
+  image(picture, 0, 0, width, height);
+}
