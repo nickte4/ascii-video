76b629556932108a0c5613c3cd700cb5344784f9
nickte4 <ntopacio25@gmail.com>
2022-07-19 23:07:31 -0700

    Convert img to 'G', greyscale

--- sketch.js
+++ sketch.js
@@ -12,5 +12,25 @@ function setup() {
 
 function draw() {
   background(220);
-  image(picture, 0, 0, width, height);
+  // image(picture, 0, 0, width, height);
+
+  let w = width / picture.width;
+  let h = height / picture.height;
+  picture.loadPixels();
+
+  for (let i = 0; i < picture.width; i++) {
+    for (let j = 0; j < picture.height; j++) {
+      const pixelIndex = (i + j * picture.width) * 4;
+      const r = picture.pixels[pixelIndex + 0];
+      const g = picture.pixels[pixelIndex + 1];
+      const b = picture.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+
+      noStroke();
+      fill(avg);
+      // square(i * w, j * h, w);
+      textSize(w);
+      text("G", i * w, j * h);
+    }
+  }
 }
