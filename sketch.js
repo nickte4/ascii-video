19c89354fbfe5f9705e31436c4c48198e74ecd95
nickte4 <ntopacio25@gmail.com>
2022-07-19 23:37:17 -0700

    Add ASCII img functionality

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._   ";
 
 let picture;
 
@@ -7,10 +7,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
+  noCanvas();
   background(0);
   // image(picture, 0, 0, width, height);
 
@@ -18,24 +15,21 @@ function draw() {
   let h = height / picture.height;
   picture.loadPixels();
 
-  for (let i = 0; i < picture.width; i++) {
-    for (let j = 0; j < picture.height; j++) {
+  for (let j = 0; j < picture.height; j++) {
+    let row = "";
+    for (let i = 0; i < picture.width; i++) {
       const pixelIndex = (i + j * picture.width) * 4;
       const r = picture.pixels[pixelIndex + 0];
       const g = picture.pixels[pixelIndex + 1];
       const b = picture.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
-
-      noStroke();
-      fill(255);
-      // square(i * w, j * h, w);
-
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, len, 0));
 
-      textSize(w);
-      textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i * w, j * h);
+      const c = density.charAt(charIndex);
+      if (c == " ") row += "&nbsp";
+      else row += c;
     }
+    createDiv(row);
   }
 }
